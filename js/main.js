3f0197b1198f71a7020304d4bf80348c81989d15
Abdull6kh <pbgpbg016@gmail.com>
2022-07-03 23:48:15 +0500

    add new functions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 let list = document.querySelector(".list")
+let form = document.querySelector(".form")
 let select = document.querySelector(".select")
+let sorSelect = document.querySelector(".sor-select")
+let input = document.querySelector(".input")
 var newarr = []
+let newSarr = []
 var sett = new Set(newarr)
+var setSort = new Set(newSarr)
 func(films, list)
 films.forEach(el => {
 	for (e of el.genres) {
@@ -25,7 +30,40 @@ select.addEventListener("change", function () {
 	})
 	func(result, list)
 })
-
+films.forEach(el => {
+	for (e of el.title[0]) {
+		setSort.add(e)
+	}
+})
+setSort.forEach(el => {
+	var newOption = document.createElement("option")
+	newOption.textContent = el
+	newOption.value = el
+	sorSelect.appendChild(newOption)
+})
+sorSelect.addEventListener("change", function () {
+	list.innerHTML = ""
+	var results = []
+	let val = sorSelect.value
+	films.forEach(elem => {
+		if (elem.title[0].includes(val)) {
+			results.push(elem)
+		}
+	})
+	func(results, list)
+})
+form.addEventListener("submit", function(evt){
+	evt.preventDefault()
+	list.innerHTML = ""
+	var resultse = []
+	let val = input.value
+	films.forEach(elem => {
+		if (elem.title.includes(val)) {
+			resultse.push(elem)
+		}
+	})
+	func(resultse, list)
+})
 function func(arr, node) {
 	for (num of arr) {
 		var newItem = document.createElement("li")
